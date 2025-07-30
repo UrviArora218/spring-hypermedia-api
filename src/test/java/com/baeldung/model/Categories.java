f98f51973b085fe0e9da885e23458abc1a9521ca
UrviArora218 <urviarora0810@gmail.com>
2025-07-30 13:19:33 +0000

    Unit test generated by RoostGPT
    Using AI Model roostgpt-4-32k

---
+++ src/test/java/com/baeldung/model/Categories.java
@@ -0,0 +1,29 @@
+package com.baeldung.model;
+
+public interface Categories {
+
+	interface valid {
+
+	}
+
+	interface invalid {
+
+	}
+
+	interface boundary {
+
+	}
+
+	interface integration {
+
+	}
+
+	interface hashCode {
+
+	}
+
+	interface equals {
+
+	}
+
+}
